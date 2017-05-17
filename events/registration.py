16755de4ceb073f0933663a1ec5e4758cefb61b9
DenisOsiflyak <Denis.Osiflyak@gmail.com>
2017-05-18 00:32:22 +0300

    remove unused files

--- events/registration.py
+++
@@ -1,6 +0,0 @@
-from api_operation import Create
-
-
-class Registration(Create):
-    def __init__(self, api_key, api_url):
-        Create.__init__(api_key=api_url,api_url=api_key,event_name= __class__.__name__)
